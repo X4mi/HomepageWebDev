4c9dbfc40acd2ae3128174d2816ecbce62702a4b
X4mi <maxi.kiermeier@gmx.de>
2021-04-05 15:11:42 +0200

    Formatting

--- Homepage/feedback.js
+++ Homepage/feedback.js
@@ -8,28 +8,28 @@ count.innerHTML = length;
 form.onreset = function () {
     count.innerHTML = length;
     progress.setAttribute("value", 0)
-    };
+};
 
 area.onkeyup = function () {
-count.innerHTML = (length - this.value.length);
-progress.setAttribute("value", Math.round((this.value.length / length * 100)))
+    count.innerHTML = (length - this.value.length);
+    progress.setAttribute("value", Math.round((this.value.length / length * 100)))
 };
 
 document.querySelector('#open-feedback').addEventListener('click', toggleDialog);
 document.querySelector('#close-feedback').addEventListener('click', toggleDialog);
-function toggleDialog(event){ 
+function toggleDialog(event) {
     var dialog = document.querySelector('#feedback');
     var closebutton = document.querySelector('#close-feedback');
     if (!dialog.hasAttribute('open')) {
-        dialog.setAttribute('open','open');
+        dialog.setAttribute('open', 'open');
     }
-    else {		
-        dialog.removeAttribute('open');  
+    else {
+        dialog.removeAttribute('open');
     }
-}  
+}
 
 function showContent() {
     var temp = document.getElementsByTagName("template")[0];
     var clone = temp.content.cloneNode(true);
     document.body.appendChild(clone);
-  }
+}
